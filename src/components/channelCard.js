abe1f7f7c3473a9ebf0589e44c544829f91b1671
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2020-05-24 02:15:04 +0545

    image enchancements with react-image

--- src/components/channelCard.js
+++ src/components/channelCard.js
@@ -1,4 +1,20 @@
 import React from 'react';
+import { useImage } from 'react-image';
+
+const Img = ({className, alt, src, onError}) => {
+  const res = useImage({
+    srcList: [src, 'https://picsum.photos/600/400.jpg?blur=6&grayscale'],
+  });
+ 
+  return (
+    <img 
+      src={res.src}
+      className = {className}
+      alt = {alt}
+      onError = {onError}
+    />
+  )
+}
 
 const ChannelCard = ({clickHandler, channel }) => {
   // backup image in case original img source fails
@@ -11,12 +27,14 @@ const ChannelCard = ({clickHandler, channel }) => {
   return (
     <div onClick={() => clickHandler(channel)} className="group cursor-pointer rounded shadow-md hover:shadow-2xl w-full h-full bg-gray-100">
       <div className='w-full h-32 p-3 flex items-center justify-center'>
-        <img 
-          className="mx-auto max-h-full" 
-          alt={channel.name+' logo'} 
-          src={channel.logo || "https://picsum.photos/500/400.jpg?blur=6&grayscale" } 
-          onError={imgErrorHandler} 
+      <React.Suspense fallback={<p>Loading...</p>}>
+        <Img
+          className = 'mx-auto max-h-full'
+          alt = {channel.name+' logo'} 
+          src = {channel.logo || "https://picsum.photos/500/400.jpg?blur=6&grayscale"}
+          onError = {imgErrorHandler}
         />
+      </React.Suspense>
       </div>
       <h5 className="group-hover:text-gray-900 text-gray-700 font-medium p-3">{channel.name}</h5>
     </div>
