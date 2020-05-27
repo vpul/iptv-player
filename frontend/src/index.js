91cdc7a6f1c412c6c0f7af5cccf4de24d06f6a8c
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2020-05-28 01:27:18 +0545

    changed folder structure for background-workers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './assets/main.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+ReactDOM.render((
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Router>
+      <Switch>
+        <Route exact path='/' component={App} />
+        {/* <Route component={NotFound} /> */}
+      </Switch>
+    </Router>,
+  </React.StrictMode>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
